129af6fcfd22101119b4f50ea5f45524ea7fe0bf
linedev06 <vitorafelismino@gmail.com>
2023-04-02 01:00:54 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int primeironumero, segundonumero, resultado;
+
+Console.WriteLine("--- Subtração ---");
+
+Console.WriteLine("Digite o primeiro numero: ");
+primeironumero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero: ");
+segundonumero = Convert.ToInt32(Console.ReadLine());
+
+resultado = primeironumero - segundonumero;
+
+Console.WriteLine($"Subtração: {resultado}");
